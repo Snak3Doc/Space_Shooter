4c084f293bd7446711bd61253be0c971e6417812
SnakeDoc <benjaminbadham@gmail.com>
2023-10-07 17:19:56 +0700

    Major Refactoring

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+# main.py
 ### Imports ###
-from typing import Any
-import pygame
-pygame.init()
 import pathlib
 import sys
-import random
+from random import choice, randint
 from time import sleep
-#* Add any other modules you need
+
+import pygame as pg
+pg.init()
 from rich import print
 from rich.traceback import install
 install(show_locals=True)
 
+### Data ###
+game_over = False
+delta_time = 0
 
-
-### Constants ###
 SCREEN_WIDTH = 720
 SCREEN_HEIGHT = 720
 
@@ -21,14 +22,20 @@ SPRITE_WIDTH = 40
 SPRITE_HEIGHT = 40
 
 FPS = 60
-FIRE_RATE = 0.5
+
+POWER_RATE = 2
+power_timer = POWER_RATE
 
 WORK_DIR = pathlib.Path.cwd()
 
-# Paths
-BG_IMG_PATH = WORK_DIR/"game_assets"/"UI"/"space_background.png"
+LARGE_FONT = pg.font.Font(WORK_DIR/"game_assets"/"game_font.ttf", 30)
+SMALL_FONT = pg.font.Font(WORK_DIR/"game_assets"/"game_font.ttf", 16)
 
-PLAYER_IMG_PATH = WORK_DIR/"game_assets"/"Players"/"playerShip1_red.png"
+#* Paths
+BG_IMG_PATH = WORK_DIR/"game_assets"/"UI"/"space_background.png"
+BG_IMG = pg.transform.scale(pg.image.load(BG_IMG_PATH), (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+PLAYER_IMG_PATH = WORK_DIR/"game_assets"/"Players"/"playerShip1_green.png"
 
 LASER_IMG_PATH = WORK_DIR/"game_assets"/"Lasers"/"laserGreen11.png"
 
@@ -45,161 +52,211 @@ METEOR_IMG_PATH_4 = WORK_DIR/"game_assets"/"Meteors"/"meteorBrown_small2.png"
 METEOR_IMG_PATH_5 = WORK_DIR/"game_assets"/"Meteors"/"meteorBrown_tiny1.png"
 METEOR_IMG_PATH_6 = WORK_DIR/"game_assets"/"Meteors"/"meteorBrown_tiny2.png"
 
-# Load Images
-BG_IMG = pygame.image.load(BG_IMG_PATH)
+HP_IMG_PATH = WORK_DIR/"game_assets"/"Power-ups"/"pill_red.png"
+FR_IMG_PATH = WORK_DIR/"game_assets"/"Power-ups"/"bolt_gold.png"
 
+LASER_SFX_PATH = WORK_DIR/"game_assets"/"Audio"/"laserSmall_002.ogg"
+EXPLOSION_SFX_PATH = WORK_DIR/"game_assets"/"Audio"/"explosionCrunch_000.ogg"
 
-### Variables ###
-game_over = False
-delta_time = 0
+#* Path Lists
+ENEMY_IMG_PATHS = [ENEMY_IMG_PATH_1, ENEMY_IMG_PATH_2, ENEMY_IMG_PATH_3,
+                   ENEMY_IMG_PATH_4, ENEMY_IMG_PATH_5]
 
-small_font = pygame.font.Font(WORK_DIR/"game_assets"/"game_font.ttf", 16)
-large_font = pygame.font.Font(WORK_DIR/"game_assets"/"game_font.ttf", 30)
-
-### Lists ###
-EMEMY_IMG_PATHS = [ENEMY_IMG_PATH_1, ENEMY_IMG_PATH_2, ENEMY_IMG_PATH_3,
-                    ENEMY_IMG_PATH_4, ENEMY_IMG_PATH_5]
-METEOR_IMG_PATHS = [METEOR_IMG_PATH_1, METEOR_IMG_PATH_2, METEOR_IMG_PATH_3, 
+METEOR_IMG_PATHS = [METEOR_IMG_PATH_1, METEOR_IMG_PATH_2, METEOR_IMG_PATH_3,
                     METEOR_IMG_PATH_4, METEOR_IMG_PATH_5, METEOR_IMG_PATH_6]
 
 
 ### Window Setup ###
-main_win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Space Shooter")
-clock = pygame.time.Clock()
+mw = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption("Space Shooter")
+clock = pg.time.Clock()
 
 ### Classes ###
-class SpriteBase(pygame.sprite.Sprite):
-    def __init__(self, img_path, x_pos, y_pos, scale_width, scale_height, step): #* Add extra parameters as needed
+class BaseSprite(pg.sprite.Sprite):
+    def __init__(self, img_path, scale_width, scale_height, x_pos, y_pos, step):
         super().__init__()
         if scale_width is not None and scale_height is not None:
-            self.image = pygame.transform.scale(pygame.image.load(img_path), (scale_width, scale_height))
+            self.image = pg.transform.scale(pg.image.load(img_path), (scale_width, scale_height))
         else:
-            self.image = pygame.image.load(img_path)
+            self.image = pg.image.load(img_path)
 
         self.rect = self.image.get_rect()
         self.rect.x = x_pos
         self.rect.y = y_pos
 
         self.step = step
-
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
-        self.top_limit = 5
-        self.bottom_limit = SCREEN_HEIGHT - self.height - 5
-        self.left_limit = 5
-        self.right_limit = SCREEN_WIDTH -  self.width - 5
-
-class PlayerSprite(SpriteBase):
-    def __init__(self, img_path, x_pos, y_pos, scale_width, scale_height, step):
-        super().__init__(img_path, x_pos, y_pos, scale_width, scale_height, step)
+class PlayerSprite(BaseSprite):
+    def __init__(self, img_path, scale_width, scale_height, x_pos, y_pos, step):
+        super().__init__(img_path, scale_width, scale_height, x_pos, y_pos, step)
         self.start_x = x_pos
         self.start_y = y_pos
-        self.shot_timer = FIRE_RATE
+
         self.lives = 3
         self.score = 0
 
-    def update(self):
-        pressed_keys = pygame.key.get_pressed()
+        self.FIRE_RATE = 0.5
+        self.shot_timer = self.FIRE_RATE
+        self.fr_power_timer = None # Initialize the timer
 
-        if pressed_keys[pygame.K_a] and self.rect.x > self.left_limit:
+    def update(self):
+        pressed_keys = pg.key.get_pressed()
+
+        if pressed_keys[pg.K_a] and self.rect.x >=0:
             self.rect.x -= self.step
 
-        if pressed_keys[pygame.K_d] and self.rect.x < self.right_limit:
+        if pressed_keys[pg.K_d] and self.rect.x <= SCREEN_WIDTH - SPRITE_WIDTH:
             self.rect.x += self.step
 
         self.shot_timer -= delta_time
-        if pressed_keys[pygame.K_SPACE] and self.shot_timer <= 0:
-            self.shot_timer = FIRE_RATE
-            laser = LaserSprite(LASER_IMG_PATH, self.rect.centerx - 3, self.rect.y - 15, 6, 36, 4)
+        if pressed_keys[pg.K_SPACE] and self.shot_timer <= 0:
+            laser_sfx.play()
+            self.shot_timer = self.FIRE_RATE
+            laser = LaserSprite(LASER_IMG_PATH, 6, 36, self.rect.centerx - 3, self.rect.y - 20, 4)
             laser_sprite_group.add(laser)
 
+        if self.FIRE_RATE != 0.5:
+            if self.fr_power_timer is None:
+                self.fr_power_timer = pg.time.get_ticks()
+            elif pg.time.get_ticks() - self.fr_power_timer >= 5000: # 5000 == 5s
+                self.FIRE_RATE = 0.5
+                self.fr_power_timer = None
 
-
-class LaserSprite(SpriteBase):
+class LaserSprite(BaseSprite):
     def update(self):
-        if self.rect.y > -57:
+        if self.rect.y > -self.rect.height:
             self.rect.y -= self.step
-            if pygame.sprite.groupcollide(laser_sprite_group, enemy_sprite_group, True, True):
+            if pg.sprite.groupcollide(laser_sprite_group, enemy_sprite_group, True, True):
+                explosion_sfx.play()
                 del self
                 player.score += 10
-                print(f"Score {player.score}")
+
         else:
+            print("Laser deleted")
             laser_sprite_group.remove(self)
             del self
 
-
-
-class EnemySprite(SpriteBase):
+class EnemySprite(BaseSprite):
     def update(self):
-        if self.rect.y < SCREEN_HEIGHT:
+        if self.rect.y <= SCREEN_HEIGHT:
             self.rect.y += self.step
-            if pygame.sprite.spritecollide(player, enemy_sprite_group, True):
+            if pg.sprite.spritecollide(player, enemy_sprite_group, True):
                 del self
                 player.lives -= 1
-                print(f"Lives {player.lives}")
-        else:
-           enemy_sprite_group.remove(self)
-           del self
-           player.lives -= 1
-           print(f"Lives {player.lives}")
 
-class MeteorSprite(SpriteBase):
-    def update(self):
-        if self.rect.y < SCREEN_HEIGHT:
-            self.rect.y += self.step
-            if pygame.sprite.groupcollide(player_sprite_group, meteor_sprite_group, True, True):
-                print("Meteor hit player")
-                player.lives = 0
-            if pygame.sprite.groupcollide(enemy_sprite_group, meteor_sprite_group, True, False):
-                print("Meteor hit enemy")
-                pass
         else:
-           meteor_sprite_group.remove(self)
-           del self
-           print("Meteor of screen")
+            print("Enemy deleted")
+            enemy_sprite_group.remove(self)
+            del self
+            player.lives -= 1
+
+class MeteorSprite(BaseSprite):
+    def update(self):
+        if self.rect.y <= SCREEN_HEIGHT:
+            self.rect.y += self.step
+            if pg.sprite.groupcollide(player_sprite_group, meteor_sprite_group, True, True):
+                explosion_sfx.play()
+                del self
+                player.lives = 0
+                print("Player hit by Meteor")
+
+            if pg.sprite.groupcollide(enemy_sprite_group, meteor_sprite_group, True, False):
+                explosion_sfx.play()
+                print("Enemy hit by Meteor")
+                pass
+
+        else:
+            print("Meteor deleted")
+            meteor_sprite_group.remove(self)
+            del self
+
+class HPPowerSprite(BaseSprite):
+    def update(self):
+        if self.rect.y <= SCREEN_HEIGHT:
+            self.rect.y += self.step
+            if pg.sprite.spritecollide(player, power_up_sprite_group, True):
+                print("Power up added to player")
+                del self
+                player.lives = 3
+
+        else:
+            print("Power up deleted")
+            power_up_sprite_group.remove(self)
+            del self
+
+class FRPowerSprite(BaseSprite):
+    def update(self):
+        if self.rect.y <= SCREEN_HEIGHT:
+            self.rect.y += self.step
+            if pg.sprite.spritecollide(player, power_up_sprite_group, True):
+                print("Power up added to player")
+                del self
+                player.FIRE_RATE = 0.2
+
+        else:
+            print("Power up deleted")
+            power_up_sprite_group.remove(self)
+            del self
 
 
 ### Functions ###
 def check_exit(event):
-    if event.type == pygame.QUIT:
-        pygame.quit()
+    if event.type == pg.QUIT:
+        pg.quit()
         sys.exit()
 
 def spawn_enemies():
     if len(enemy_sprite_group) < 5:
-        enemy = EnemySprite(
-            random.choice(EMEMY_IMG_PATHS),
-            random.randint(SPRITE_WIDTH, SCREEN_WIDTH - SPRITE_WIDTH * 2),
-            -(random.randint(SPRITE_HEIGHT, SPRITE_HEIGHT * 6)),
-            SPRITE_WIDTH, SPRITE_HEIGHT,
-            random.randint(1, 2))
+        enemy = EnemySprite(choice(ENEMY_IMG_PATHS),
+                            SPRITE_WIDTH, SPRITE_HEIGHT,
+                            randint(SPRITE_WIDTH, SCREEN_WIDTH - SPRITE_WIDTH * 2),
+                            -(randint(SPRITE_HEIGHT, SPRITE_HEIGHT * 6)),
+                            randint(1, 2))
         enemy_sprite_group.add(enemy)
 
 def spawn_meteors():
     if len(meteor_sprite_group) < 2:
-        meteor = MeteorSprite(
-            random.choice(METEOR_IMG_PATHS),
-            random.randint(SPRITE_WIDTH, SCREEN_WIDTH - SPRITE_WIDTH * 2),
-            -(random.randint(SPRITE_HEIGHT, SPRITE_HEIGHT * 6)),
-            None, None,
-            random.randint(2, 4))
+        meteor = MeteorSprite(choice(METEOR_IMG_PATHS),
+                            None, None,
+                            randint(SPRITE_WIDTH, SCREEN_WIDTH - SPRITE_WIDTH * 2),
+                            -(randint(SPRITE_HEIGHT, SPRITE_HEIGHT * 6)),
+                            randint(2, 4))
         meteor_sprite_group.add(meteor)
 
-def draw_lives():
-    text_image = small_font.render(f"{str(player.lives)} Lives", True, (214, 214, 214))
-    main_win.blit(text_image, (600, 10))
+def spawn_power(POWER_RATE, power_timer, delta_time):
+    power_timer -= delta_time
+    #!print(f"Power Timer: {power_timer}")
+    if power_timer <= 0:
+        power_timer = POWER_RATE
+        spawn = bool(randint(0, 1))
+        print(f"Spawn = {spawn}")
+        if spawn:
+            power_type = randint(0, 1)
+            if power_type == 0:
+                power_up = HPPowerSprite(HP_IMG_PATH,
+                                    None, None,
+                                    randint(SPRITE_WIDTH, SCREEN_WIDTH - SPRITE_WIDTH * 2),
+                                    -(randint(SPRITE_HEIGHT, SPRITE_HEIGHT * 6)),
+                                    randint(2, 4))
+                power_up_sprite_group.add(power_up)
+            elif power_type == 1:
+                power_up = FRPowerSprite(FR_IMG_PATH,
+                                    None, None,
+                                    randint(SPRITE_WIDTH, SCREEN_WIDTH - SPRITE_WIDTH * 2),
+                                    -(randint(SPRITE_HEIGHT, SPRITE_HEIGHT * 6)),
+                                    randint(2, 4))
+                power_up_sprite_group.add(power_up)
 
-def draw_score():
-    text_image = small_font.render(f"Score {str(player.score)}", True, (214, 214, 214))
-    main_win.blit(text_image, (15, 10))
+    return power_timer
+
 
 def check_game_over():
     if player.lives <= 0:
-        text_image_1 = large_font.render(f"Game Over!", True, (214, 214, 214))
-        text_image_2 = large_font.render(f"Total Score", True, (214, 214, 214))
-        text_image_3 = large_font.render(f"{str(player.score)}", True, (214, 214, 214))
+        text_image_1 = LARGE_FONT.render(f"Game Over!", True, (214, 214, 214))
+        text_image_2 = LARGE_FONT.render(f"Total Score", True, (214, 214, 214))
+        text_image_3 = LARGE_FONT.render(f"{player.score}", True, (214, 214, 214))
 
         text_rect_1 = text_image_1.get_rect()
         text_rect_2 = text_image_2.get_rect()
@@ -209,59 +266,72 @@ def check_game_over():
         text_rect_2.center = (SCREEN_WIDTH/2, 390)
         text_rect_3.center = (SCREEN_WIDTH/2, 440)
 
-        main_win.blit(text_image_1, text_rect_1)
-        main_win.blit(text_image_2, text_rect_2)
-        main_win.blit(text_image_3, text_rect_3)
 
-        pygame.display.update()
+        mw.blit(text_image_1, text_rect_1)
+        mw.blit(text_image_2, text_rect_2)
+        mw.blit(text_image_3, text_rect_3)
+
+        pg.display.update()
         sleep(2)
-        pygame.quit()
+        pg.quit()
         sys.exit()
 
-### Objects & Groups ###
-# Groups
-player_sprite_group = pygame.sprite.Group()
-enemy_sprite_group = pygame.sprite.Group()
-laser_sprite_group = pygame.sprite.Group()
-meteor_sprite_group = pygame.sprite.Group()
+def draw_score():
+    text_image = SMALL_FONT.render(f"Score {player.score}", True, (214, 214, 214))
+    mw.blit(text_image, (15, 10))
+
+def draw_lives():
+    text_image = SMALL_FONT.render(f"{player.lives} Lives", True, (214, 214, 214))
+    mw.blit(text_image, (600, 10))
 
 
-# Sprites
-player = PlayerSprite(PLAYER_IMG_PATH, 360, 660, SPRITE_WIDTH, SPRITE_HEIGHT, 5)
+### Objects and Groups ###
+#* Groups
+player_sprite_group = pg.sprite.Group()
+laser_sprite_group = pg.sprite.Group()
+enemy_sprite_group = pg.sprite.Group()
+meteor_sprite_group = pg.sprite.Group()
+power_up_sprite_group = pg.sprite.Group()
 
-# Add Sprites to Groups
+#* Sprites
+player = PlayerSprite(PLAYER_IMG_PATH, SPRITE_WIDTH, SPRITE_HEIGHT, 360, 650, 5)
+
+#*Adding Sprites to groups
 player_sprite_group.add(player)
 
+#* Audio
+laser_sfx = pg.mixer.Sound(LASER_SFX_PATH)
+explosion_sfx = pg.mixer.Sound(EXPLOSION_SFX_PATH)
 
-### Audio ###
-#* Use pygame.mixer.music.load(PATH_TO_MUSIC) to load background music
-#* Use sound_effect_name = pygame.mixer.Sound(PATH_TO_SOUND_EFFECT) to add sound effect objects
-#* Use pygame.mixer.music.play() to play background music
 
 ### Game Loop ###
 while not game_over:
-    main_win.blit(BG_IMG, (0, 0))
-    
+    mw.blit(BG_IMG, (0, 0))
+
     check_game_over()
     draw_lives()
     draw_score()
 
     spawn_enemies()
     spawn_meteors()
+    power_timer = spawn_power(POWER_RATE, power_timer, delta_time)
 
     laser_sprite_group.update()
     player_sprite_group.update()
     enemy_sprite_group.update()
     meteor_sprite_group.update()
+    power_up_sprite_group.update()
+    
 
-    laser_sprite_group.draw(main_win)
-    player_sprite_group.draw(main_win)
-    enemy_sprite_group.draw(main_win)
-    meteor_sprite_group.draw(main_win)
+    laser_sprite_group.draw(mw)
+    player_sprite_group.draw(mw)
+    enemy_sprite_group.draw(mw)
+    meteor_sprite_group.draw(mw)
+    power_up_sprite_group.draw(mw)
 
-    for event in pygame.event.get():
+
+    for event in pg.event.get():
         check_exit(event)
 
-    pygame.display.update()
-    delta_time = clock.tick(FPS)/1000
-
+    pg.display.update()
+    delta_time = clock.tick(FPS) / 1000
